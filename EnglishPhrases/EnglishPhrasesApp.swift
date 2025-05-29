332d8dffc2093cbe4c8aa2064871711feab85070
Ayşe Geçgel <aysegecgel94@gmail.com>
2025-05-29 12:03:01 +0300

    Initial Commit

---
+++ EnglishPhrases/EnglishPhrasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishPhrasesApp.swift
+//  EnglishPhrases
+//
+//  Created by ayse GECGEL on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct EnglishPhrasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnglishPhrasesTests/EnglishPhrasesTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishPhrasesTests.swift
+//  EnglishPhrasesTests
+//
+//  Created by ayse GECGEL on 29.05.2025.
+//
+
+import Testing
+@testable import EnglishPhrases
+
+struct EnglishPhrasesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
